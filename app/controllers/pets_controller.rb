ed6b619b97d0e9f7920515e73407f2f55dbfcdb2
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-28 19:09:03 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,38 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pets = Pet.create(params[:pet])
+    if !params["pet"]["name"].empty? #here we are creating an if statement to check if [pet name] is empty
+      @owner.pets << Pet.create(name: params["pet"]["name"]) #then shoveling the data into the owner instance
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
+    ####### bug fix
+    if !params[:pet].keys.include?("pet_ids")
+    params[:pet]["pet_ids"] = []
+    end
+    #######
 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
