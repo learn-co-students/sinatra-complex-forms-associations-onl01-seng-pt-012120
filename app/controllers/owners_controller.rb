79e5e918bbbfcd7d6875114ee3f91db12f5fa433
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-28 19:24:41 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,14 +12,18 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
-    if !params["pet"]["name"].empty? #here we are creating an if statement to check if [pet name] is empty
-      @owner.pets << Pet.create(name: params["pet"]["name"]) #then shoveling the data into the owner instance
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+      # When using the shovel operator, ActiveRecord instantly fires update SQL
+      # without waiting for the save or update call on the parent object,
+      # unless the parent object is a new record.
     end
-    redirect "owners/#{@owner.id}"
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -29,17 +33,13 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    ####### bug fix
-    if !params[:owner].keys.include?("pet_ids")
-    params[:owner]["pet_ids"] = []
-    end
-    #######
-
     @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
+    @owner.update(params[:owner])
+
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect "owners/#{@owner.id}"
+
+    redirect "/owners/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,10 +10,14 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pets = Pet.create(params[:pet])
-    if !params["pet"]["name"].empty? #here we are creating an if statement to check if [pet name] is empty
-      @owner.pets << Pet.create(name: params["pet"]["name"]) #then shoveling the data into the owner instance
+    @pet = Pet.create(params[:pet])
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+
+    @pet.save
+
     redirect to "pets/#{@pet.id}"
   end
 
@@ -22,18 +26,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do
-    ####### bug fix
-    if !params[:pet].keys.include?("pet_ids")
-    params[:pet]["pet_ids"] = []
-    end
-    #######
-
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
-    @pet.update(params["pet"])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+
+    #binding.pry
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+
+    @pet.save
+
     redirect to "pets/#{@pet.id}"
   end
 end
